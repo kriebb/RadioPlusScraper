99c0171cf21a1d308c98423d6a58fca7b9df76fd
kriebb <kristof.riebbels@recomatics.com>
2018-07-23 10:13:52 +0200

    Add project files.

---
+++ RadioPlusScraper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RadioPlusScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
