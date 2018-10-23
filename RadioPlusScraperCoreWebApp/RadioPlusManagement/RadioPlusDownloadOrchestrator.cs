390b3e164dc070327a29f278e5a75fb56775cf99
kriebb <kristof.riebbels@recomatics.com>
2018-10-23 11:17:33 +0200

    setting private method DoScheduleJob public, because Hangfire can't start a private method as a job. Refactoring as a class maybe?

--- RadioPlusScraperCoreWebApp/RadioPlusManagement/RadioPlusDownloadOrchestrator.cs
+++ RadioPlusScraperCoreWebApp/RadioPlusManagement/RadioPlusDownloadOrchestrator.cs
@@ -18,14 +18,15 @@ namespace RadioPlusScraperWebApi
         public void Start()
         {
             var backGroundClient = new BackgroundJobClient();
-            var startDockerJobId = backGroundClient.Create(() => _dockerContainerHandler.Start(), new EnqueuedState(_radioPlusDownloadHandler.NextTimeSpan.ToString().Replace(":","_")));
-            var getInfoJobId = backGroundClient.ContinueWith(startDockerJobId, () => _radioPlusDownloadHandler.Start(), new EnqueuedState(_radioPlusDownloadHandler.NextTimeSpan.ToString().Replace(":","_")));
-            var stopDockerJobId = backGroundClient.ContinueWith(getInfoJobId, () => _dockerContainerHandler.Stop(), new EnqueuedState(_radioPlusDownloadHandler.NextTimeSpan.ToString().Replace(":","_")));
-            backGroundClient.ContinueWith(stopDockerJobId, () => DoScheduleJob(backGroundClient), new EnqueuedState(_radioPlusDownloadHandler.NextTimeSpan.ToString().Replace(":","_")));
+            var startDockerJobId = backGroundClient.Create(() => _dockerContainerHandler.Start(), new EnqueuedState(_radioPlusDownloadHandler.NextTimeSpan.ToString().Replace(":", "_")));
+            var getInfoJobId = backGroundClient.ContinueWith(startDockerJobId, () => _radioPlusDownloadHandler.Start(), new EnqueuedState(_radioPlusDownloadHandler.NextTimeSpan.ToString().Replace(":", "_")));
+            var stopDockerJobId = backGroundClient.ContinueWith(getInfoJobId, () => _dockerContainerHandler.Stop(), new EnqueuedState(_radioPlusDownloadHandler.NextTimeSpan.ToString().Replace(":", "_")));
+            backGroundClient.ContinueWith(stopDockerJobId, () => DoScheduleJob(), new EnqueuedState(_radioPlusDownloadHandler.NextTimeSpan.ToString().Replace(":", "_")));
         }
 
-        private void DoScheduleJob(BackgroundJobClient backGroundClient)
+        public void DoScheduleJob()
         {
+            var backGroundClient = new BackgroundJobClient();
             backGroundClient.Schedule(() => Start(), _radioPlusDownloadHandler.NextTimeSpan);
         }
 
